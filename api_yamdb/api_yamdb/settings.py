3c543a0ca0d4d00dad1b2b2d7848213c16fd0059
vtarasov <vtarasov@nvidia.com>
2023-06-14 15:45:21 +0300

    adding code fore reviews endpoint

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,6 +11,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
+} 
+
 
 # Application definition
 
