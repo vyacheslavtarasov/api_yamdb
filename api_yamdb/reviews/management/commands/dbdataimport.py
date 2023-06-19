47b3b3418cac97fb5a865a608ba4a75fd1f7b06f
vtarasov <vtarasov@nvidia.com>
2023-06-19 13:43:22 +0300

    one more try

--- api_yamdb/reviews/management/commands/dbdataimport.py
+++ api_yamdb/reviews/management/commands/dbdataimport.py
@@ -81,12 +81,12 @@ class Command(BaseCommand):
 
             if columns_to_add:
                 for column in columns_to_add:
-                    (name, default_value) = column
+                    name, default_value = column
                     df.insert(0, name, default_value)
 
             if columns_date_modification_type:
                 for column in columns_date_modification_type:
-                    (name, format) = column
+                    name, format = column
                     df[name] = pandas.to_datetime(df[name], format=format)
 
             df.to_sql(table_name, conn, if_exists="append", index=False)
